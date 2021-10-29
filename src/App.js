482c65321fee1f5c5ddf35472a4d85a909d67e1e
manish kumar <epicvila.care@gmail.com>
2021-10-29 23:36:40 +0530

    broken into smaller component and also finished styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.components';
-import MenuItem from './components/menu-item/menu-item.component';
 
 function App() {
   return (
     <div>
-      <MenuItem />
       <HomePage />
     </div>
   );
